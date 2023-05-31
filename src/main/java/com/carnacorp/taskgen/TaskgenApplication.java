390be6cab3aafa118a9681e1d17f828d71868b7f
Leocarnauba <leocarmacedo@hotmail.com>
2023-05-31 14:37:01 -0300

    Initial config

---
+++ src/main/java/com/carnacorp/taskgen/TaskgenApplication.java
@@ -0,0 +1,13 @@
+package com.carnacorp.taskgen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskgenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskgenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carnacorp/taskgen/TaskgenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carnacorp.taskgen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskgenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
